c381bc5c69e6f7fa54350b15c397c8890b683923
Futsuiii <89948802+Futsuiii@users.noreply.github.com>
2022-10-09 21:54:12 -0300

    09/10

--- Projeto - Estacionamento/frontend/js/script.js
+++ Projeto - Estacionamento/frontend/js/script.js
@@ -23,6 +23,8 @@ function carregar() {
 function preencherVagas() {
    
     var tamanho = 0
+    var achou = false
+    var ocupadas = 0
     vagas.map(e => [
         
     ])
@@ -31,48 +33,160 @@ function preencherVagas() {
         if (i <= 50) {
 
             var paragrao1 = document.createElement('p')
-            paragrao1.innerHTML = i
-            document.querySelector('.mini1').appendChild(paragrao1)
-            tamanho += 1
+
+            achou = false
             
+            // var paragrao = document.createElement('p')
+
             vagas.forEach(e => {
                 if(i == e.id_vaga) {
-                    if(e.categoria_vaga == 'Ve?culo Pequeno')
-                    paragrao1.classList = 'bx bx-cycling'
-                    paragrao1.id = 'teste'
-                    
+                    // if(e.categoria_vaga == 'Ve?culo Pequeno')
+                        
+                        // paragrao1.id = 'teste'
+                        
+                        achou = true
+
+                        tipo = e.categoria_vaga
+                        
+                        
                 }
             })
+
+            if(achou == true){
+
+                if(tipo == 'Ve?culo Pequeno') {
+                    paragrao1.classList = 'bx bx-cycling'
+                    document.querySelector('.mini1').appendChild(paragrao1)
+                    ocupadas+= 1
+                }
+                else if (tipo == 'Ve?culo M?dio') {
+                    paragrao1.classList = 'bx bx-car'
+                    document.querySelector('.mini1').appendChild(paragrao1)
+                    ocupadas+= 1
+                }
+
+                else if (tipo == 'Ve?culo Grande') {
+                    paragrao1.classList = 'bx bxs-truck'
+                    document.querySelector('.mini1').appendChild(paragrao1)
+                    ocupadas+= 1
+                }
+                
+                
+            }
+            else {
+                paragrao1.innerHTML = i
+                document.querySelector('.mini1').appendChild(paragrao1)
+                tamanho += 1
+
+            }
+
+            
+            
+            
+            
         }
         else if (i >= 50 && i <= 100) {
+
             var paragrao2 = document.createElement('p')
-            paragrao2.innerHTML = i
-            document.querySelector('.mini2').appendChild(paragrao2)
-            tamanho += 1
+            achou = false
+            
+            // var paragrao = document.createElement('p')
+
             vagas.forEach(e => {
                 if(i == e.id_vaga) {
-                paragrao2.style.background = "blue"
-                    
+                    // if(e.categoria_vaga == 'Ve?culo M?dio')
+                        
+                        // paragrao1.id = 'teste'
+                        
+                        achou = true
+                        tipo = e.categoria_vaga
+
+                        
                 }
             })
+
+            if(achou == true){
+
+                if(tipo == 'Ve?culo Pequeno') {
+                    paragrao2.classList = 'bx bx-cycling'
+                    document.querySelector('.mini2').appendChild(paragrao2)
+                    ocupadas+= 1
+                }
+                else if (tipo == 'Ve?culo M?dio') {
+                    paragrao2.classList = 'bx bx-car'
+                    document.querySelector('.mini2').appendChild(paragrao2)
+                    ocupadas+= 1
+                }
+
+                else if (tipo == 'Ve?culo Grande') {
+                    paragrao2.classList = 'bx bxs-truck'
+                    document.querySelector('.mini2').appendChild(paragrao2)
+                    ocupadas+= 1
+                }
+                
+                
+            }
+            else {
+                paragrao2.innerHTML = i
+                document.querySelector('.mini2').appendChild(paragrao2)
+                tamanho += 1
+
+            }
         }
         else {
             var paragrao3 = document.createElement('p')
-            paragrao3.innerHTML = i
-            document.querySelector('.mini3').appendChild(paragrao3)
-            tamanho += 1
+            achou = false
+            
+            // var paragrao = document.createElement('p')
+
             vagas.forEach(e => {
                 if(i == e.id_vaga) {
-                paragrao3.style.background = "green"
-                    
+                    // if(e.categoria_vaga == 'Ve?culo Grande')
+                        
+                        // paragrao1.id = 'teste'
+
+                        achou = true
+                        
+                        tipo = e.categoria_vaga
+
                 }
             })
-        }
+
+            if(achou == true){
+
+                if(tipo == 'Ve?culo Pequeno') {
+                    paragrao3.classList = 'bx bx-cycling'
+                    document.querySelector('.mini3').appendChild(paragrao3)
+                    ocupadas+= 1
+                }
+                else if (tipo == 'Ve?culo M?dio') {
+                    paragrao3.classList = 'bx bx-car'
+                    document.querySelector('.mini3').appendChild(paragrao3)
+                    ocupadas+= 1
+                }
+
+                else if (tipo == 'Ve?culo Grande') {
+                    paragrao3.classList = 'bx bxs-truck'
+                    document.querySelector('.mini3').appendChild(paragrao3)
+                    ocupadas+= 1
+                }
+                
+                
+            }
+            else {
+                paragrao3.innerHTML = i
+                document.querySelector('.mini3').appendChild(paragrao3)
+                tamanho += 1
+
+            }
     
     }
     
+}
+    
     document.querySelector('.qtd-vagas').innerHTML = tamanho
     document.querySelector('.vagas-livres').innerHTML = tamanho
+    document.querySelector('.vagas-Ocupadas').innerHTML = ocupadas
     
 
     
