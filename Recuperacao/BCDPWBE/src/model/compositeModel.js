2c93d0329313bb929cd64027d71c2b08cebae0ee
ElianMoraes <elianmoreas9@gmail.com>
2022-12-08 11:17:00 -0300

    08/12

--- Recuperacao/BCDPWBE/src/model/compositeModel.js
+++ Recuperacao/BCDPWBE/src/model/compositeModel.js
@@ -1,33 +1,33 @@
 
-const osComposite = (array) => {
+// const osComposite = (array) => {
 
-    let ordemServicoList = []
+//     let ordemServicoList = []
 
-    array.forEach(e => {
-        let curso = {}
-        curso.aluno = {}
-        curso.curso = {}
+//     array.forEach(e => {
+//         let curso = {}
+//         curso.aluno = {}
+//         curso.curso = {}
 
-        curso.aluno['id'] = e.id_aluno
-        curso.aluno['nome'] = e.nome
-        curso.aluno['nascimento'] = e.nascimento
+//         curso.aluno['id'] = e.id_aluno
+//         curso.aluno['nome'] = e.nome
+//         curso.aluno['nascimento'] = e.nascimento
 
-        curso.curso['id'] = e.id_curso
-        curso.curso['curso'] = e.curso
-        curso.curso['duracao'] = e.duracao
+//         curso.curso['id'] = e.id_curso
+//         curso.curso['curso'] = e.curso
+//         curso.curso['duracao'] = e.duracao
 
-        curso['data'] = e.data
+//         curso['data'] = e.data
 
-        cursosList.push(curso);
+//         cursosList.push(curso);
         
-    });
+//     });
 
-    return ordemServicoList
+//     return ordemServicoList
    
     
-}
+// }
 
 
-module.exports = {
-    osComposite
-}
+// module.exports = {
+//     osComposite
+// }
